1e48b241773ed2380d21c371f381858677240279
Evgeny Maruschenko <Evgeny.Maruschenko@x5.ru>
2022-09-24 22:45:30 +1000

    Add grid column

--- src/state.lua
+++ src/state.lua
@@ -37,12 +37,34 @@ function Param:add_value(delta)
     self.value = math.min(math.max(self.min, value), self.max)
 end
 
+---@class GridColumn
+---@field ch Param midi channel 1-16.
+---@field cc Param midi control change 0-119.
+---@field value Param midi control change value 0-127.
+GridColumn = {
+    ch = Param:new { value = 1, min = 1, max = 16 },
+    cc = Param:new { value = 0, min = 0, max = 119 },
+    value = Param:new { value = 0, min = 0, max = 127 },
+}
+---@return GridColumn
+function GridColumn:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
 ---@class State
 ---@field key table<keys, KeyState> state of keys.
----@field enc table<encoders, Param> value of encoders.
+---@field grid_columns table<integer, table> column of grid.
 State = {
     key = { KeyState:new(), KeyState:new(), KeyState:new(), },
-    enc = { Param:new(), Param:new(), Param:new(), },
+    grid_columns = {
+        GridColumn:new(),
+        GridColumn:new(),
+        GridColumn:new(),
+        GridColumn:new(),
+    },
 }
 ---@return State
 function State:new(o)
